221623203eb395163befb9aa8ba7118b1b676965
LautaroQuintana33 <lnquintana@fiu.uba.ar>
2026-09-17 11:19:29 -0300

    Cargo la base de datos junto a la estructura basica de la api

---
+++ db/init_db.sql
@@ -0,0 +1,190 @@
+CREATE DATABASE  IF NOT EXISTS `CLUB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `CLUB`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: CLUB
+-- ------------------------------------------------------
+-- Server version	8.4.11-0ubuntu0.26.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `canchas`
+--
+
+DROP TABLE IF EXISTS `canchas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `canchas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `deporte_id` int NOT NULL,
+  `precio_hora` int NOT NULL,
+  `importe` int NOT NULL,
+  `techada` tinyint(1) NOT NULL,
+  `activa` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_d` (`deporte_id`),
+  CONSTRAINT `fk_d` FOREIGN KEY (`deporte_id`) REFERENCES `deportes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `canchas`
+--
+
+LOCK TABLES `canchas` WRITE;
+/*!40000 ALTER TABLE `canchas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `canchas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deportes`
+--
+
+DROP TABLE IF EXISTS `deportes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `deportes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deportes`
+--
+
+LOCK TABLES `deportes` WRITE;
+/*!40000 ALTER TABLE `deportes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `deportes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estados_reservas`
+--
+
+DROP TABLE IF EXISTS `estados_reservas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estados_reservas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estados_reservas`
+--
+
+LOCK TABLES `estados_reservas` WRITE;
+/*!40000 ALTER TABLE `estados_reservas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estados_reservas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estados_socios`
+--
+
+DROP TABLE IF EXISTS `estados_socios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estados_socios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estados_socios`
+--
+
+LOCK TABLES `estados_socios` WRITE;
+/*!40000 ALTER TABLE `estados_socios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estados_socios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservas`
+--
+
+DROP TABLE IF EXISTS `reservas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `socio_id` int NOT NULL,
+  `fecha_hora_inicio` datetime NOT NULL,
+  `fecha_hora_fin` datetime NOT NULL,
+  `importe` int NOT NULL,
+  `estado_id` int NOT NULL,
+  `cancha_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_socio_id_` (`socio_id`),
+  KEY `fk_cancha_id` (`cancha_id`),
+  KEY `fk_estado_reservas_id` (`estado_id`),
+  CONSTRAINT `fk_cancha_id` FOREIGN KEY (`cancha_id`) REFERENCES `canchas` (`id`),
+  CONSTRAINT `fk_estado_reservas_id` FOREIGN KEY (`estado_id`) REFERENCES `estados_reservas` (`id`),
+  CONSTRAINT `fk_socio_id` FOREIGN KEY (`socio_id`) REFERENCES `socios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservas`
+--
+
+LOCK TABLES `reservas` WRITE;
+/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `socios`
+--
+
+DROP TABLE IF EXISTS `socios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `socios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `estado_id` int NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_estado_id` (`estado_id`),
+  CONSTRAINT `fk_estado_id` FOREIGN KEY (`estado_id`) REFERENCES `estados_socios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `socios`
+--
+
+LOCK TABLES `socios` WRITE;
+/*!40000 ALTER TABLE `socios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `socios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-09-16 21:06:50
