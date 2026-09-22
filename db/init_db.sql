74fad284733ff64d9c6d53b875ee6cdb8f0f8448
LautaroQuintana33 <lnquintana@fiu.uba.ar>
2026-09-22 14:10:39 -0300

    Añado la creacion de la base a init_db.sql y agrego el id a la tabla bloqueos

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `CLUB` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `CLUB`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: CLUB
@@ -23,11 +25,12 @@ DROP TABLE IF EXISTS `bloqueos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bloqueos` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `cancha_id` int NOT NULL,
   `fecha` date NOT NULL,
   `horario_inicio` time NOT NULL,
   `horario_fin` time NOT NULL,
-  `motivo` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
   KEY `cancha_id` (`cancha_id`),
   CONSTRAINT `cancha_id` FOREIGN KEY (`cancha_id`) REFERENCES `canchas` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -214,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-19 20:17:52
+-- Dump completed on 2026-09-22 14:09:10
